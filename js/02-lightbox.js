65340ea509c390e34a60042c6a03ec001f12f9b8
Viktoria <vik25@i.ua>
2022-12-26 13:34:27 +0100

    task#2 finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryArr = document.querySelector(".gallery");
+
+
+const images = galleryItems.map(({ preview, original, description }) =>
+`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a>`).join("")
+
+galleryArr.insertAdjacentHTML('beforeend', images)
+
+function handleClick(event) {
+    //прибираємо дію браузера за замовчуванням
+    event.preventDefault();
+    lightboxFunction()
+}
+
+function lightboxFunction() {
+
+  let gallery = new SimpleLightbox('.gallery a', {fadeSpeed: 250});
+    gallery.on('show.simplelightbox', function () {
+    });
+   
+}
+
+galleryArr.addEventListener("click", handleClick);
+
+// Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс
+// cdnjs.Необхідно додати посилання на два файли:
+// simple - lightbox.min.js і simple - lightbox.min.css.
+
+// Ініціалізація бібліотеки після створення і додання елементів
+//  галереї у div.gallery.Для цього ознайомся з документацією
+// SimpleLightbox - насамперед секції «Usage» і «Markup».
+
+// Подивися в документації секцію «Options» і додай відображення
+//  підписів до зображень з атрибута alt.Нехай підпис буде знизу
+//   і з'являється через 250 мілісекунд після відкриття зображення.
--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,10 +1,10 @@
 export const galleryItems = [
-  {
+   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
     original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-    description: 'Hokkaido Flower',
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
   },
   {
     preview:
